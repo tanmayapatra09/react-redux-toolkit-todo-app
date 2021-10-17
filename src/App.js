026c3f4c83fe4a052d5388dcde624713cddd3cda
Tanmaya Patra <tanmayapatra09@gmail.com>
2021-10-17 15:39:19 +0530

    A small todo task

--- src/App.js
+++ src/App.js
@@ -1,56 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { saveTodo } from './features/todoSlice'
 
 function App() {
+
+  const [input, setInput] = useState("");
+
+  const todoList = useSelector((state) => state.todos.todoList)
+
+  const dispatch = useDispatch();
+
+  const addTodo = () => {
+    dispatch(saveTodo({
+      item: input,
+      done: false,
+      id: Date.now()
+    }))
+    setInput("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Todo List</h1>
+      {todoList.map((todo, index) => <p key={index}>Hi {todo.item} </p>)}
+      <h1>Add new Todo</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <input type="button" onClick={addTodo} value="Add" />
     </div>
   );
 }
